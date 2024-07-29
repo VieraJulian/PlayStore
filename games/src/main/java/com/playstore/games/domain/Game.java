d551cfb068a7506a371c455b7d8fb7154440cf58
VieraJulian <julianviera2019@hotmail.com>
2024-07-29 19:16:04 -0300

    Creación del inputport y dtos

--- games/src/main/java/com/playstore/games/domain/Game.java
+++ games/src/main/java/com/playstore/games/domain/Game.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.SQLDelete;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
