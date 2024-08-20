30d0dc8794e47b0b6482e7710e66d66543124f00
VieraJulian <julianviera2019@hotmail.com>
2024-08-20 20:05:40 -0300

    Entidades creadas, faltan las relaciones

---
+++ orders/src/main/java/com/playstore/orders/domain/Order.java
@@ -0,0 +1,47 @@
+package com.playstore.orders.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import org.hibernate.annotations.SQLDelete;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SQLDelete(sql = "UPDATE orders SET enabled = false WHERE id = ?")
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private Long user_id;
+    @Column(nullable = false)
+    private BigDecimal final_price;
+    @Column(nullable = false)
+    private LocalDate date_purchase;
+    @Column(nullable = false)
+    private String code_operation;
+    @Column(nullable = false)
+    private boolean enabled;
+
+    @OneToMany
+    private List<OrderItem> ordersItems;
+
+}
